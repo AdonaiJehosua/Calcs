994ff3002873839b88c66b1f4cae850bdfa648d6
adonaijehosua <adonaijehosua@gmail.com>
2022-06-21 23:07:12 +0500

    SOme things

--- client/src/structure/routes/Test/Test.js
+++ client/src/structure/routes/Test/Test.js
@@ -1,9 +1,19 @@
 export const Test = () => {
 
 
+    const selectAll = () => {
+        let selectedText = document.getElementById('select')
+        selectedText.select()
+        // let range = new Range()
+        // range.selectNode(selectedText)
+        // console.log(selectedText)
+        // console.log(range)
+    }
+
     return (
         <div>
-
+            <input id={'select'} type={'text'}/>
+            <button onClick={selectAll}>SelectAll</button>
         </div>
     )
 }
