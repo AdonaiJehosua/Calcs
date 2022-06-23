1feddeb2c58d3a24d6286474e97b3d07ca65b4b6
adonaijehosua <adonaijehosua@gmail.com>
2022-06-24 01:04:56 +0500

    add router.put - changing enrty with simple validation

--- routes/unit.routes.js
+++ routes/unit.routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express')
 const Unit = require('../models/Unit')
 const {check, validationResult} = require('express-validator')
 const auth = require('../middleware/auth.middleware')
+const validator = require("validator");
 const router = Router()
 
 router.post(
@@ -52,6 +53,52 @@ router.delete('/:id', auth, async (req, res) => {
         res.status(500).json({message: `Что-то пошло не так, попробуйте снова ${req.params.id}`})
     }
 })
+
+router.put('/:id/changeentryvalue', auth,
+
+    async (req, res) => {
+        try {
+
+            const {updatingValue} = req.body
+
+            switch (req.body.entryKey) {
+                case 'fullName':
+                    if (validator.isEmpty(updatingValue)) {
+                        return (res.status(400).json({message: `Введите полное название`}))
+                    }
+
+                    const examinationFullName = await Unit.findOne({fullName: updatingValue})
+                    if (examinationFullName) {
+                        return res.status(400).json({message: `Единица измерения с таким названием существует - ${updatingValue}`})
+                    }
+
+                    await Unit.updateOne({_id: req.params.id}, {$set: {fullName : updatingValue}})
+                    res.status(201).json({message: 'Полное название изменено.', updatedValue: updatingValue})
+
+                    break
+
+                case 'abbreviatedName':
+                    if (validator.isEmpty(updatingValue)) {
+                        return (res.status(400).json({message: `Введите сокращенное название`}))
+                    }
+
+                    const examinationAbbreviatedName = await Unit.findOne({abbreviatedName: updatingValue})
+                    if (examinationAbbreviatedName) {
+                        return res.status(400).json({message: `Единица измерения с таким названием существует - ${updatingValue}`})
+                    }
+
+                    await Unit.updateOne({_id: req.params.id}, {$set: {abbreviatedName : updatingValue}})
+                    res.status(201).json({message: 'Сокращенное название изменено.', updatedValue: updatingValue})
+
+                    break
+
+            }
+
+
+        } catch (e) {
+            res.status(500).json({message: `Что-то пошло не так, попробуйте снова`})
+        }
+    })
 
 router.get('/', auth, async (req, res) => {
     try {
