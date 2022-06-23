38ce0bcbfc49e118b11509e271a85bcdbd078302
adonaijehosua <adonaijehosua@gmail.com>
2022-06-24 01:02:52 +0500

    some tests

--- client/src/structure/routes/Test/Test.js
+++ client/src/structure/routes/Test/Test.js
@@ -1,19 +1,34 @@
+import {useEffect} from "react";
+import M from 'materialize-css'
+
 export const Test = () => {
 
-
-    const selectAll = () => {
-        let selectedText = document.getElementById('select')
-        selectedText.select()
-        // let range = new Range()
-        // range.selectNode(selectedText)
-        // console.log(selectedText)
-        // console.log(range)
-    }
+    useEffect(() => {
+        M.AutoInit()
+    }, [])
 
     return (
         <div>
-            <input id={'select'} type={'text'}/>
-            <button onClick={selectAll}>SelectAll</button>
+            {/*<nav>*/}
+            {/*    <div className="nav-wrapper">*/}
+            {/*        <a href="#!" className="brand-logo">Logo</a>*/}
+            {/*        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i*/}
+            {/*            className="material-icons">menu</i></a>*/}
+            {/*        <ul className="right hide-on-med-and-down">*/}
+            {/*            <li><a href="sass.html">Sass</a></li>*/}
+            {/*            <li><a href="badges.html">Components</a></li>*/}
+            {/*            <li><a href="collapsible.html">Javascript</a></li>*/}
+            {/*            <li><a href="mobile.html">Mobile</a></li>*/}
+            {/*        </ul>*/}
+            {/*    </div>*/}
+            {/*</nav>*/}
+
+            {/*<ul className="sidenav" id="mobile-demo">*/}
+            {/*    <li><a href="sass.html">Sass</a></li>*/}
+            {/*    <li><a href="badges.html">Components</a></li>*/}
+            {/*    <li><a href="collapsible.html">Javascript</a></li>*/}
+            {/*    <li><a href="mobile.html">Mobile</a></li>*/}
+            {/*</ul>*/}
         </div>
     )
 }
