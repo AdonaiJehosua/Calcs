d59be920ef9d072406d89292804caa029d9fd72b
AdonaiJehosua <adonaijehosua@gmail.com>
2022-07-14 17:05:54 +0500

    Refactor a little

--- client/src/components/SelectComponent.js
+++ client/src/components/SelectComponent.js
@@ -3,7 +3,19 @@ import {useCallback, useEffect, useState} from "react";
 import {MenuItem, TextField} from "@mui/material";
 
 
-export const SelectComponent = ({addItemName, addItemValue, label, nameKey, endpoint, initialKey, touched, values, errors, handleChange, handleBlur}) => {
+export const SelectComponent = ({
+                                    addItemName,
+                                    addItemValue,
+                                    label,
+                                    nameKey,
+                                    endpoint,
+                                    initialKey,
+                                    touched,
+                                    values,
+                                    errors,
+                                    handleChange,
+                                    handleBlur
+                                }) => {
 
     const {entries, fetchEntries} = useFetchEntries()
     const [loaded, setLoaded] = useState(false)
@@ -18,21 +30,21 @@ export const SelectComponent = ({addItemName, addItemValue, label, nameKey, endp
     }, [fetchValues])
 
     if (loaded) return (
-            <TextField select fullWidth
-                       error={touched[initialKey] && Boolean(errors[initialKey])}
-                       helperText={touched[initialKey] && errors[initialKey]}
-                       id={initialKey}
-                       name={initialKey}
-                       value={values[initialKey]}
-                       label={label}
-                       onChange={handleChange}
-                       onBlur={handleBlur}>
-                {addItemName && <MenuItem value={addItemValue}>{addItemName}</MenuItem>}
-                    {entries.map((el) => {
-                        return (
-                            <MenuItem key={el._id} value={el._id}>{el[nameKey]}</MenuItem>
-                        )
-                    })}
-            </TextField>
+        <TextField select fullWidth
+                   error={touched[initialKey] && Boolean(errors[initialKey])}
+                   helperText={touched[initialKey] && errors[initialKey]}
+                   id={initialKey}
+                   name={initialKey}
+                   value={values[initialKey]}
+                   label={label}
+                   onChange={handleChange}
+                   onBlur={handleBlur}>
+            {addItemName && <MenuItem value={addItemValue}>{addItemName}</MenuItem>}
+            {entries.map((el) => {
+                return (
+                    <MenuItem key={el._id} value={el._id}>{el[nameKey]}</MenuItem>
+                )
+            })}
+        </TextField>
     )
 }
