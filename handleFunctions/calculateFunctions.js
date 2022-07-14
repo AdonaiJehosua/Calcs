4c804f352687130bc57ac718afb4bb119b79b521
AdonaiJehosua <adonaijehosua@gmail.com>
2022-07-14 17:05:02 +0500

    Create new function - sheetParts

--- handleFunctions/calculateFunctions.js
+++ handleFunctions/calculateFunctions.js
@@ -13,3 +13,31 @@ module.exports.examFormatsAreas = function (lesserFormat, largerFormat) {       
     return Boolean((lesserFormat.height * lesserFormat.width) < (largerFormat.height * largerFormat.width))       /*   Функция возвращает true, если формат, который должен быть меньше, действительно меньше формата, который должен быть больше.   */
 }                                                                                                                       /*   lesserFormat - формат, который должен быть меньше. largerFormat - формат, который должен быть больше.                         */
                                                                                                                         /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+module.exports.sheetParts = function (lesserFormat,
+                                      largerFormat,
+                                      grainDirection = false,
+                                      isLesserFormatLongSideGrainDirection,
+                                      isLargerFormatLongSideGrainDirection) {
+    switch (grainDirection) {
+        case false:
+            if (Math.floor(largerFormat.height / lesserFormat.height) >=
+                Math.floor(largerFormat.width / lesserFormat.height)) {
+                return (Math.floor(largerFormat.height / lesserFormat.height) *
+                    Math.floor(largerFormat.width / lesserFormat.width))
+            }
+            return (Math.floor(largerFormat.width / lesserFormat.height) *
+                Math.floor(largerFormat.height / lesserFormat.width))
+
+        case true:
+            const examGrainDirectionMatches = Boolean(isLesserFormatLongSideGrainDirection ===
+                isLargerFormatLongSideGrainDirection)
+
+            if (examGrainDirectionMatches === true) {
+                return (Math.floor(largerFormat.height / lesserFormat.height) *
+                    Math.floor(largerFormat.width / lesserFormat.width))
+            }
+            return (Math.floor(largerFormat.height / lesserFormat.width) *
+                Math.floor(largerFormat.width / lesserFormat.height))
+    }
+}
