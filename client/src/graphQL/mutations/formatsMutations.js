e1ca58fc7f1eb6c5ad3d0315d288fd1100355963
adonaijehosua <adonaijehosua@gmail.com>
2022-08-12 00:05:22 +0500

    add mutations for formats

--- client/src/graphQL/mutations/formatsMutations.js
+++ client/src/graphQL/mutations/formatsMutations.js
@@ -3,4 +3,11 @@ import {gql} from "@apollo/client";
 export const ADD_FORMAT = gql`
     mutation ($formatName: String!, $dimensions: InputDimensions!) {
         message: addFormat (formatName: $formatName, dimensions: $dimensions)
-    }`
+    }
+`
+
+export const DELETE_FORMAT = gql`
+    mutation ($id: ID) {
+        message: deleteFormat (id: $id)
+    }
+`
