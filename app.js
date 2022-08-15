1e6487c5636959d50eacb275bcdfab159e330f27
adonaijehosua <adonaijehosua@gmail.com>
2022-08-16 02:10:47 +0500

    remove unused imports

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
-const {graphqlHTTP} = require('express-graphql')
-// const schema = require('./graphQLSchema/schema')
-const cors = require('cors')
 const {createServer} = require('http')
 const {ApolloServerPluginDrainHttpServer,
     ApolloServerPluginLandingPageLocalDefault} = require('apollo-server-core');
@@ -19,7 +16,6 @@ const wsServer = new WebSocketServer({
     path: '/graphql',
 });
 const serverCleanup = useServer({ schema }, wsServer);
-
 
 const server = new ApolloServer({
     schema,
@@ -40,33 +36,12 @@ const server = new ApolloServer({
     ]
 })
 
-
-// app.use(cors())
-
-// app.use(express.json({extended: true}))
-
-// app.use('/graphql', graphqlHTTP({
-//     schema,
-//     graphiql: true
-// }))
-
-
-
-
-
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/format', require('./routes/catalogs/format.routes'))
-app.use('/api/unit', require('./routes/catalogs/unit.routes'))
-app.use('/api/chromaticity', require('./routes/catalogs/chromaticity.routes'))
-app.use('/api/calcs', require('./routes/calcs/amountOfPaper.routes'))
-
 const PORT = config.get('port') || 5000
 
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
         })
-        // app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
         await server.start()
         server.applyMiddleware({ app })
         httpServer.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
