71cc57afad8f62fa189108f5a0b5ee346aebf3b5
adonaijehosua <adonaijehosua@gmail.com>
2022-08-16 02:18:26 +0500

    add refetchQueries

--- client/src/hooks/toastedMutation.hook.js
+++ client/src/hooks/toastedMutation.hook.js
@@ -6,8 +6,11 @@ import {useEffect} from "react";
 // Метод response в свою очередь, принимает: 1. Объект переменных для мутации *ОБЯЗАТЕЛЬНО*. 2 - собственно, объект значений формика *ОБЯЗАТЕЛЬНО*. 2 - массив представленных строками идентификаторов значений, которые должны обнуляться в инпутах при успешном создании документа *ОПЦИОНАЛЬНО* - на случай, если обнулять нужно не все значения формы.
 // Если мутация успешна, сообщение из ответа пуляется во всплывающее сообщение. Если имела место ошибка, она пуляется во всплывающее сообщение об ошибке. Тиакие пироги.
 
-export const useToastedMutation = (query) => {
-    const [mutation, {data}] = useMutation(query)
+export const useToastedMutation = (gqlMutation, gqlQuery, queryName) => {
+    const [mutation, {data}] = useMutation(gqlMutation, {refetchQueries: [
+            {query: gqlQuery},
+            queryName
+        ]})
     const makeMutation = async (variables, values, resetValues) => {
         try {
             await mutation({variables: variables})
